aeb2eb21ed3a9c4ccc278eb2a9ab7a97a2a7f206
HeLLo2 <popt0@naver.com>
2023-11-28 23:57:18 +0900

    temp

--- programmers/temp/test.java
+++ programmers/temp/test.java
@@ -3,34 +3,11 @@ package programmers.temp;
 import java.util.Objects;
 
 public class test {
-
-    public static class Point {
-        int x;
-        int y;
-
-        public Point() {
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Point point = (Point) o;
-            return x == point.x && y == point.y;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
-        }
-    }
-
     public static void main(String[] args) {
+        char a = 'b';
+        char b = 'c';
 
-        Point point = new Point();
-        System.out.println(point.hashCode());
-
+        System.out.println();
     }
-
 }
 
